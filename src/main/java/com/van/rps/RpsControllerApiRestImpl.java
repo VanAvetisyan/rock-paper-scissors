9dbb6758327e67fc570691cef139e459c495db90
Van Avetisyan <van.etseuv@gmail.com>
2022-11-20 20:20:34 +0100

    [RPS-001] Adding some Logs

--- src/main/java/com/van/rps/RpsControllerApiRestImpl.java
+++ src/main/java/com/van/rps/RpsControllerApiRestImpl.java
@@ -3,6 +3,8 @@ package com.van.rps;
 import com.van.rps.model.UserPickRequest;
 import com.van.rps.model.UserPickResponse;
 import com.van.rps.support.RpsControllerRestSupport;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,22 +12,17 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.rmi.ServerException;
-
-import static java.util.Objects.isNull;
-
 @RestController
 public class RpsControllerApiRestImpl {
+    private static final Logger logger = LoggerFactory.getLogger(RpsControllerApiRestImpl.class);
     @Autowired
     private RpsControllerRestSupport rpsControllerRestSupport;
 
     @PostMapping("/api/rest/rps/userpick")
-    public ResponseEntity<UserPickResponse> userPickedObjectWinnerCalculator(@RequestBody UserPickRequest request) throws ServerException {
+    public ResponseEntity<UserPickResponse> userPickedObjectWinnerCalculator(@RequestBody UserPickRequest request) {
+        logger.info("User {} asked for response with {} pick", request.getUserName(), request.getUserPick());
         UserPickResponse userPickResponse = rpsControllerRestSupport.calculatePicksAndWinner(request);
-        if (isNull(request)) {
-            throw new ServerException("Error when returning data");
-        } else {
-            return new ResponseEntity<>(userPickResponse, HttpStatus.OK);
-        }
+        logger.info("Returning data");
+        return new ResponseEntity<>(userPickResponse, HttpStatus.OK);
     }
 }
